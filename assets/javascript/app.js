93b58575ada6a65467b0830868b655cb557af4f8
Alberto Vargas <a.vargasmarte@gmail.com>
2018-01-05 22:58:52 -0500

    started pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,33 @@
+//Basic Trivia Quiz (Timed Form) Pseudocode
+
+/*The following describes my approach to code the 
+basic trivia quiz based on the hw instructions:
+
+* You'll create a trivia form with multiple choice or true/false options (your choice).
+
+I have a few ideas for trivia themes:
+    - Nutrition myths
+    - Dominican history
+    - Geography
+    - Nerdy, sciency topics (maybe with a focus on public health)
+    - Languages
+
+These questions will be a combination of T/F and MC
+
+In the form, I will use radio button input types to make sure that the
+player can only choose one of the answer options
+
+* The player will have a limited amount of time to finish the quiz. 
+
+Depending on the length/theme of the game I will set a timer with 
+a range of 30-120 seconds. I will use the setTimeout() method to
+execute a function after the time elapses. This function will 
+trigger a 'game over' screen that will display the number of 
+correct/incorrect answers 
+
+  * The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+
+* Don't let the player pick more than one answer per question.
+
+* Don't forget to include a countdown timer.
+    */
